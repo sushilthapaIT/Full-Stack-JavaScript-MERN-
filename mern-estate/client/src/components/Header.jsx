b0a705198dd2d030b77bf27e03c03db13902e110
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-15 00:34:40 -0400

    Created Header Component
    -- adding UI and style

---
+++ mern-estate/client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-slate-500'>Sushi</span>
+        <span className='text-slate-500'>Estate</span>
+      </h1>
+    </header>
+  )
+}
