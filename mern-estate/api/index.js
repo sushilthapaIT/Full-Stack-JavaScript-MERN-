ade97c8324a3de750ed9b6fe7c12bbf931481947
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-16 18:58:21 -0400

    Created Middleware and a function to handle possible error

--- mern-estate/api/index.js
+++ mern-estate/api/index.js
@@ -20,3 +20,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRouter);
 app.use('/api/user', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode, // after es6 we variable and key have same name we use only variable name
+        message,
+    });
+}); //next for going to next middleware, err us for errir, req is request from user, res is from server
