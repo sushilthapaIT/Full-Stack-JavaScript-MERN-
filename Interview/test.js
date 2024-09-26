258aad75498df2a90de20d002c25cee730fcaf54
Sushil Thapa <87489909+sushilthapaIT@users.noreply.github.com>
2024-09-25 23:14:33 -0400

    update

---
+++ Interview/test.js
@@ -0,0 +1,18 @@
+// global scope
+var a = 5;
+console.log(a);
+
+
+// function scope
+function functionScope() {
+    var t = 6;
+    console.log(t)
+}
+
+
+
+// block scope
+{
+    let p = 6;
+    console.log
+}
